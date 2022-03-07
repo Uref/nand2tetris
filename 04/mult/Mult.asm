f3dce9da5c5c3b3da11475deff99aabc3292559b
YusukeSK <62171006+YusukeSK@users.noreply.github.com>
2022-03-07 13:54:23 +0900

    finish ch.4

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  @i
+  M=0
+  @2
+  M=0
+
+(LOOP)
+  @i
+  D=M
+  @1
+  D=D-M
+  @END
+  D; JGE
+  @i
+  M=M+1
+  @0
+  D=M
+  @2
+  M=D+M
+  @LOOP
+  0; JMP
+(END)
+  @END
+  0; JMP
